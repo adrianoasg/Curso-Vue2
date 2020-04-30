919d56111805437bcf07d5b7a87a3d1922bd0064
adrianoasg <adrianoasg@hotmail.com>
2020-04-30 14:45:45 -0300

    modulo7-Introducao-components

---
+++ 7-introducao-components/componente-desafio/src/main/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-app>
+		<Menu />
+		<Toolbar />
+		<v-content>
+			<Carousel />
+		</v-content>
+		<Footer />
+	</v-app>
+</template>
+
+<script>
+import Menu from '@/components/template/Menu.vue' //caminho global
+import Toolbar from '@/components/template/Toolbar.vue'
+import Carousel from '../components/widgets/Carousel.vue' //caminho relativo
+import Footer from '../components/template/Footer.vue'
+
+export default {
+	components: { Footer, Carousel, Toolbar, Menu },
+}
+</script>
