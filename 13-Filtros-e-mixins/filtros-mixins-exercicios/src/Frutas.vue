098e82aa7d9a0954b52a646c58afd3b5af185c77
adrianoasg <adrianoasg@hotmail.com>
2020-05-06 15:00:37 -0300

    filtros-mixins

---
+++ 13-Filtros-e-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ global }}</h1>
+    <ul>
+      <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+    </ul>
+    <input type="text" v-model="fruta" @keydown.enter="add">
+  </div>
+</template>
+
+<script>
+import FrutasMixin from './frutasMixin'
+import UsuarioMixin from './usuarioMixin'
+
+export default {
+  mixins: [FrutasMixin, UsuarioMixin],
+  created() {
+    console.log('Created - Frutas.vue!')
+  }
+}
+</script>
+
+<style>
+
+</style>
