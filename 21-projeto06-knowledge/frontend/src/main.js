a624b0b5b04b6cdd633d953d44cb4bea7761afbc
adrianoasg <adrianoasg@hotmail.com>
2020-05-21 10:45:45 -0300

    modulo21-projeto6-componente_home

--- 21-projeto06-knowledge/frontend/src/main.js
+++ 21-projeto06-knowledge/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFkcmlhbm8gR3VpbWFyw6NlcyIsImVtYWlsIjoiYWRyaWFubm8tYWdAYm9sLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE1OTAwNjc5ODQsImV4cCI6MTU5MDMyNzE4NH0.EDVt6rYFtwCyuNRCJ35lhy4RdHaOxkYxdPcn6ZFh-ck'
+
 new Vue({
   store,
   router,
