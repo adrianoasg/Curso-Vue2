c0068c4db681b64a6cc8d4e3eb2e58bf8da7a8bb
adrianoasg <adrianoasg@hotmail.com>
2020-05-22 14:29:08 -0300

    modulo21-projeto6-autenticacão_alterando_store

--- 21-projeto06-knowledge/frontend/src/main.js
+++ 21-projeto06-knowledge/frontend/src/main.js
@@ -10,9 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkFkcmlhbm8gR3VpbWFyYWVzIiwiZW1haWwiOiJhZHJpYW5vYXNnQGhvdG1haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5MDA3NTg4OCwiZXhwIjoxNTkwMzM1MDg4fQ.ZygJSmK_TdY2Myp80GNf7IfkltHNS7I22KGEYlrvDLs'
-
 new Vue({
   store,
   router,
